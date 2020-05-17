6603930abcb70fe4c73dd98d81870c3214bbec2b
fangfelipe <fangfelipe@gmail.com>
2020-05-17 20:07:47 +0800

    update__init

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class App extends Component { // => Component
+  render() {
+    return (
+      <h1>hello React</h1>
+    )
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  entry : './index.jsx',
+  output : {
+    path: path.join(__dirname, '/dist'),
+    filename : 'bundle.[hash].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?x$/, 
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: './index.html' // => 以主目錄的 index 為範本
+    })
+  ]
+}
